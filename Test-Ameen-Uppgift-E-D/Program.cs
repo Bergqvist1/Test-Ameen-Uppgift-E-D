ad9a4de6001a7c9a27fdb8c60df096692e3d1960
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-02-17 10:35:09 +0100

    Add project files.

---
+++ Test-Ameen-Uppgift-E-D/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift_E_D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 2 heltal till en rektangel!");
+            Console.WriteLine("Skriv in höjden:");
+            int höjd = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in bredden:");
+            int bas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Rektangels area = " + Area(bas, höjd));
+        }
+        static int Area(int bas, int höjd)
+        {
+            
+            int Arean = bas * höjd;
+            return Arean;
+      
+        }
+    }
+}
